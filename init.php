4c93dc1e70ca353bffb335df150bbe9824537ec1
ahmed12hesham <38839663+ahmed12hesham@users.noreply.github.com>
2020-03-24 00:20:00 +0200

    Add files via upload

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php 
+    include 'connect.php';
+    // Routes 
+
+    $tpl = 'includes/templates/';           //Template Directory
+    $css = 'layout/css/';                   //Css Directory
+    $js  = 'layout/js/';                    //Js Directory
+    $lan = 'includes/languages/';           //Language Directory
