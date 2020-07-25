7c6531ed198893bae508f6e582c9e8d8002d25c2
ahmed12hesham <38839663+ahmed12hesham@users.noreply.github.com>
2020-07-25 04:38:28 +0200

    Delete init.php

--- init.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include 'connect.php';
-    // Routes 
-
-    $tpl = 'includes/templates/';           //Template Directory
-    $css = 'layout/css/';                   //Css Directory
-    $js  = 'layout/js/';                    //Js Directory
-    $lan = 'includes/languages/';           //Language Directory
